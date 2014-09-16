ed06441435b4c83b64eb86a0a6bb21a65b457672
MBlagui <mehdi@blagui.com>
2014-09-16 13:05:51 +0200

    Update backupuserfiles.sh

--- backupuserfiles.sh
+++ backupuserfiles.sh
@@ -19,7 +19,7 @@ exec > >(tee $logfile)
 exec 2>&1
 rm -rf old_$logfile
 mv $logfile $oldlogfile
-echo "Starting backup $(date +"%d-%b-%Y %H:%M")"> $logfile
+echo "Starting backup $(date +"%d-%b-%Y %H:%M")">> $logfile
 
 # Function function_backupdb_execute
 # Dump mysql DB and compress it into backups folder
@@ -27,48 +27,45 @@ echo "Starting backup $(date +"%d-%b-%Y %H:%M")"> $logfile
 # $3 = host ( could remove host but can work as remote backup too! )
 # $4 = database name to backup $5 = path
 function_backup_db_execute() {
-backupfolder="$5/backups"
-mkdir -p $backupfolder
-mysqldump --user=$1 --password=$2 --host=$3 $4 > $backupfolder/$4_db_$date.sql
-zip -r $backupfolder/$4_db_$date.zip $backupfolder/$4_db_$date.sql
-rm -f $backupfolder/$4_db_$date.sql
+	backupfolder="$5/backups"
+	mkdir -p $backupfolder
+	mysqldump --user=$1 --password=$2 --host=$3 $4 > $backupfolder/$4_db_$date.sql
+	zip -r $backupfolder/$4_db_$date.zip $backupfolder/$4_db_$date.sql
+	rm -f $backupfolder/$4_db_$date.sql
 } 
 # Function backup public_html folder zip
 function_backup_html() {
-   echo "html path backup now: $1/public_html"
-   zip -r $panelroot/$1/backups/$1_html_$1_$date.zip $panelroot/$1/public_html/*
+	echo "html path backup now: $1/public_html"
+	zip -r $panelroot/$1/backups/$1_html_$1_$date.zip $panelroot/$1/public_html/*
 } 
 function_backup_clear() {
-# Delete files older than 45 days
-find $1/$2_db_*.zip -mtime +$clearday -exec rm -f {} \;
-find $1/$2_html_*.zip -mtime +$clearday -exec rm -f {} \;
-echo "Removing old backups in $1 user $2" 
+	# Delete files older than 45 days
+	find $1/$2_db_*.zip -mtime +$clearday -exec rm -f {} \;
+	find $1/$2_html_*.zip -mtime +$clearday -exec rm -f {} \;
+	echo "Removing old backups in $1 user $2" 
 } 
 
 # Function backup DB get list of DB then call DB dump
 function_backup_db() {
-   echo "user ID to backup : $1"
-   mysql zpanel_core -u root -p$mysqlpassword -e "SELECT my_name_vc FROM x_mysql_databases WHERE my_acc_fk='$1' AND my_deleted_ts IS NULL;;"| while read my_name_vc; do 
-   if [ ! "$my_name_vc" == "my_name_vc" ]; then
-   echo "database to backup $my_name_vc"
-   function_backup_db_execute root $mysqlpassword localhost $my_name_vc $2
-   fi
-   done
+	echo "user ID to backup : $1"
+	mysql zpanel_core -u root -p$mysqlpassword -e "SELECT my_name_vc FROM x_mysql_databases WHERE my_acc_fk='$1' AND my_deleted_ts IS NULL;;"| while read my_name_vc; do 
+	if [ ! "$my_name_vc" == "my_name_vc" ]; then
+		echo "database to backup $my_name_vc"
+		function_backup_db_execute root $mysqlpassword localhost $my_name_vc $2
+	fi
+done
 } 
 
-#Get all users
+# Get all users from mySQL DB
 mysql zpanel_core -u root -p$mysqlpassword -e "SELECT ac_id_pk, ac_user_vc, CONCAT('/var/zpanel/hostdata/' , ac_user_vc) as ac_user_path,  CONCAT( CONCAT('/var/zpanel/hostdata/' , ac_user_vc),'/public_html') as ac_user_path_tobackup FROM x_accounts WHERE ac_deleted_ts IS NULL and ac_enabled_in = 1 ;" | while read ac_id_pk ac_user_vc ac_user_path guid ac_user_path_tobackup; do
-#Always skip first line returned by mysql...
-
-#echo $ac_user_path $ac_id_pk $ac_user_vc
-#if [  "$ac_user_vc" == "zadmin" ]; then
-# test working only for zadmin......
+# Always skip first line returned by mysql...
 if [ ! "$ac_user_path" == "ac_user_path" ]; then
-echo "Path: $ac_user_path user to backup"
-function_backup_html $ac_user_vc > $logfile
-function_backup_db $ac_id_pk $ac_user_path > $logfile
-function_backup_clear "$ac_user_path/backups" $ac_user_vc > $logfile
+	# if [  "$ac_user_vc" == "zadmin" ]; then # test working only for zadmin......
+	echo "Path: $ac_user_path user to backup"
+	function_backup_html $ac_user_vc >> $logfile
+	function_backup_db $ac_id_pk $ac_user_path >> $logfile
+	function_backup_clear "$ac_user_path/backups" $ac_user_vc >> $logfile
 fi
 done
-echo "Finished backup $(date +"%d-%b-%Y %H:%M")"> $logfile
+echo "Finished backup $(date +"%d-%b-%Y %H:%M")">> $logfile
 echo "All backup actions logged in $logfile and errors in $errorlogfile"
