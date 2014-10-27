1f2230d86ce0bbb6768233d99fe80771c7808cca
MBlagui <mehdi@blagui.com>
2014-10-27 11:54:42 +0100

    Create webmin_centos_install.sh
    install webmin on centos.
    
    Use: bash webmin_centos_install.sh

---
+++ webmin_centos_install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+mkdir /tmp/webmin
+cd /tmp/webmin
+wget http://www.webmin.com/jcameron-key.asc
+rpm --import jcameron-key.asc
+cat > /etc/yum.repos.d/webmin.repo << EOF
+[Webmin]
+name=Webmin Distribution Neutral
+baseurl=http://download.webmin.com/download/yum
+mirrorlist=http://download.webmin.com/download/yum/mirrorlist
+enabled=1
+EOF
+yum install webmin -y
