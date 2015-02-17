df533c0966185692d069b14ae1339a248a718606
MBlagui <mehdi@blagui.com>
2015-02-17 11:46:54 +0100

    Update suhosin_0.9.37.1.sh

--- suhosin_0.9.37.1.sh
+++ suhosin_0.9.37.1.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
+SUHOSIN="0.9.37.1"
 cd /tmp
-wget -nv -O suhosin.zip https://github.com/stefanesser/suhosin/archive/0.9.37.1.zip
+wget -nv -O suhosin.zip https://github.com/stefanesser/suhosin/archive/$SUHOSIN.zip
 unzip -q suhosin.zip
 rm -f suhosin.zip
-cd suhosin-0.9.37.1
+cd suhosin-$SUHOSIN
 phpize 
 ./configure 
 make
 make install 
 cd ..
-rm -rf suhosin-0.9.37.1
+rm -rf suhosin-$SUHOSIN
